fb2887c8fe9f5ff99cefe146fdbbec855306cab6
Jannis Konopka <jakon17@student.sdu.dk>
2017-10-09 13:35:35 +0200

    first commit

---
+++ Programming/build/CMakeFiles/ManEx2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ManEx2.dir/src/pathplanning.cpp.o"
+  "ManEx2.pdb"
+  "ManEx2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ManEx2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
